5f0e5f99326c53e15dbb3dc336bebb12d6a5dfbc
Abhijeet kumar <abhijeetkv2@gmail.com>
2024-06-23 11:05:56 +0530

    Done

---
+++ Hover-Effect/src/App.tsx
@@ -0,0 +1,13 @@
+
+import { CardHoverEffectDemo } from './components/ui/CardHoverEffectDemo'
+
+function App() {
+
+  return (
+    <>
+      <CardHoverEffectDemo />
+    </>
+  )
+}
+
+export default App
